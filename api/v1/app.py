557d57444e766396570c724f807d86d556b73bae
Mwysam <samuelmwy@gmail.com>
2022-05-30 15:19:39 -0700

    chmod it all

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
+"""
+flask app
+"""
 
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
+
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_appcontext(self):
-storage.close()
+    storage.close()
+    
+@app.errorhandler(404)
+def not_found(error):
+    """ returns a JSON-formatted 404 status code response """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__" :
-app.run (host='0.0.0.0', port=5000, threaded =True, debug=True)
+    app.run (host='0.0.0.0', port=5000, threaded =True, debug=True)
 
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python 3
+"""
+Resourses for flask app
+"""
 
 from flask import Flask, Blueprint
 from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+
 app = Flask(__name__)
+
 app_views = Blueprint('app_views',__name__,url_prefix="/api/v1")
 
